802498112d5653a24049d03b2b0d5bcf12fda717
Eishaan Siingh <69831841+DrOverK1ll@users.noreply.github.com>
2020-08-31 11:46:40 +0530

    Add files via upload

---
+++ adding_num.py
@@ -0,0 +1,7 @@
+try:
+    num1 = int(input('Enter the 1st Number '))
+    num2 = int(input('Enter the 2nd Number '))
+    sum = num1 + num2
+    print('The sum is ' + str(sum))
+except:
+    print('Please Enter a Valid Value!')
