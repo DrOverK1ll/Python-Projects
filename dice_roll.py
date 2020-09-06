4425631acdb9ed9ebe8449dc70f5b35e7c66a6d6
Eishaan Siingh <69831841+DrOverK1ll@users.noreply.github.com>
2020-09-06 10:13:04 +0530

    Update dice_roll.py

--- dice_roll.py
+++ dice_roll.py
@@ -1,13 +1,20 @@
 import random
 
-dice_roll = random.randint(1, 6)
+no_ofDices = int(input('How many dices do you want me to roll? '))
+
 decision = input('Do you want to play? ')
 
+while decision == decision.lower() == 'yes' or decision.lower() == 'y':
+    dice_roll = random.randint(1, 6 * no_ofDices)
 
-if decision.lower() == 'yes' or decision.lower() == 'y':
-    print("Your number is- ")
-    print(dice_roll)
-else:
-    print('''You did not enter a valid value.
-    Or exited the game. 
-    Enjoy your day!! ''')
+    if decision.lower() == 'yes' or decision.lower() == 'y':
+        print("Your number is- ")
+        print(dice_roll)
+    a = input('Do you want to play again? ')
+    if a ==  'yes' or a == 'y':
+        continue
+    else:
+        print('''You did not enter a valid value.
+        Or exited the game. 
+        Enjoy your day!! ''')
+        break 
